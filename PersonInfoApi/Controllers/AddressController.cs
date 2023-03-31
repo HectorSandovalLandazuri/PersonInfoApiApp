b4cbd7b89c107f790fd6e05ca1361b9851a33f67
HectorSandovalLandazuri <88155761+HectorSandovalLandazuri@users.noreply.github.com>
2023-03-31 10:47:56 -0600

    Agregar archivos de proyecto.

---
+++ PersonInfoApi/Controllers/AddressController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PersonInfoApi.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PersonInfoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AddressController : ControllerBase
+    {
+        private ILogger<AddressController> _logger;
+
+        public AddressController(ILogger<AddressController> logger)
+        {
+            _logger = logger;
+        }
+        // POST api/<AddressController>
+        [HttpPost]
+        public void Post([FromBody] AddressModel data)
+        {
+            _logger.LogInformation("The Address is {Address}", data);
+        }
+
+    }
+}
---
+++ PersonInfoApi/Controllers/PersonController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PersonInfoApi.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PersonInfoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        private ILogger<PersonController> _logger;
+
+        public PersonController(ILogger<PersonController> logger)
+        {
+            _logger = logger;
+        }
+        // POST api/<PersonController>
+        [HttpPost]
+        public void Post([FromBody] PersonModel data)
+        {
+            _logger.LogInformation("The Person was logged as {Person}", data);
+        }
+
+    }
+}
